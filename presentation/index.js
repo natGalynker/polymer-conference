385c3f5b265e937b23b4fa1a9a3320cfbfd656f4
natGalynker <natalgal@gmail.com>
2016-09-11 21:06:24 -0400

    Modify slides
    modify index.js
    
    remove the pizza table slide
    create list for polymer core, polymer elements, polymer tools
    change fade to slide

--- presentation/index.js
+++ presentation/index.js
@@ -16,10 +16,11 @@ require("normalize.css");
 require("../src/themes/default/index.css");
 
 const images = {
-  city: require("../assets/city.jpg"),
-  kat: require("../assets/kat.png"),
+  bench: require("../assets/fall-benches.jpg"),
+  diagram: require("../assets/architecture-diagram.png"),
   logo: require("../assets/formidable-logo.svg"),
   markdown: require("../assets/markdown.png")
+
 };
 
 preloader(images);
@@ -35,156 +36,98 @@ export default class Presentation extends React.Component {
         <Deck transition={["zoom", "slide"]} transitionDuration={500}>
           <Slide transition={["zoom"]} bgColor="primary">
             <Heading size={1} fit caps lineHeight={1} textColor="black">
-              Spectacle
+              Polymer
             </Heading>
             <Heading size={1} fit caps>
-              A ReactJS Presentation Library
+              A new Google technology library
             </Heading>
             <Heading size={1} fit caps textColor="black">
-              Where You Can Write Your Decks In JSX
+            Where you can create your own custom elements
             </Heading>
-            <Link href="https://github.com/FormidableLabs/spectacle">
-              <Text bold caps textColor="tertiary">View on Github</Text>
+            <Link href="http://pubnub.github.io/paper-chat/">
+              <Text bold caps textColor="tertiary">Here is an Example in action built by Tomomi </Text>
             </Link>
-            <Text textSize="1.5em" margin="20px 0px 0px" bold>Hit Your Right Arrow To Begin!</Text>
           </Slide>
           <Slide transition={["slide"]} bgColor="black" notes="You can even put notes on your slide. How awesome is that?">
-            <Image src={images.kat.replace("/", "")} margin="0px auto 40px" height="293px"/>
-            <Heading size={2} caps fit textColor="primary" textFont="primary">
-              Wait what?
+          <Heading size={2} caps fit textColor="primary" textFont="primary">
+          Polymer Architecture
+          </Heading>
+            <Image src={images.diagram.replace("/", "")} margin="0px auto 40px" height="500px"/>
+          </Slide>
+          <Slide transition={["slide"]} bgImage={images.bench.replace("/", "")} bgDarken={0.75} textColor="tertiary">
+            <Appear fid="1">
+            <Heading size={1} caps fit textColor="primary">
+            Polymer Tools
             </Heading>
+            </Appear>
+            <Appear><ListItem>Manipulating The Shadow Dom</ListItem></Appear>
+            <Appear><ListItem>Creating Custom Elements</ListItem></Appear>
+            <Appear><ListItem>Using Polymer Elements Library</ListItem></Appear>
+            <Appear><ListItem>Working with HTML Imports</ListItem></Appear>
+          </Slide>
+          <Slide transition={["fade"]} bgColor="navy" textColor="primary">
+          <Heading size={1} capts fit textColor="primary">
+            Polymer Core
+          </Heading>
+            <List>
+              <Appear><ListItem>Event Handlers in HTML</ListItem></Appear>
+              <Appear><ListItem>AJAX calls in HTML</ListItem></Appear>
+              <Appear><ListItem>Data binding </ListItem></Appear>
+              <Appear><ListItem>Accessing Googles APIs</ListItem></Appear>
+            </List>
           </Slide>
           <Slide transition={["zoom", "fade"]} bgColor="primary" notes="<ul><li>talk about that</li><li>and that</li></ul>">
+          <Heading size={4} textColor="black">
+            Example Polymer Event Handlers
+          </Heading>
+            <CodePane
+              lang="jsx"
+              source={require("raw!../assets/handlers.example")}
+              margin="20px auto"
+            />
+          </Slide>
+          <Slide transition={["zoom", "fade"]} bgColor="primary" notes="<ul><li>talk about that</li><li>and that</li></ul>">
+          <Heading size={4} textColor="black">
+            Example Polymer data-binding
+          </Heading>
             <CodePane
               lang="jsx"
               source={require("raw!../assets/deck.example")}
               margin="20px auto"
             />
           </Slide>
-          <Slide transition={["slide"]} bgImage={images.city.replace("/", "")} bgDarken={0.75}>
-            <Appear fid="1">
-              <Heading size={1} caps fit textColor="primary">
-                Full Width
-              </Heading>
-            </Appear>
-            <Appear fid="2">
-              <Heading size={1} caps fit textColor="tertiary">
-                Adjustable Darkness
-              </Heading>
-            </Appear>
-            <Appear fid="3">
-              <Heading size={1} caps fit textColor="primary">
-                Background Imagery
-              </Heading>
-            </Appear>
+          <Slide transition={["fade"]} bgColor="primary" notes="<ul><li>talk about that</li><li>and that</li></ul>">
+          <Heading size={4} textColor="black">
+            Example Polymer google API AJAX
+          </Heading>
+            <CodePane
+              lang="jsx"
+              source={require("raw!../assets/youtube.example")}
+              margin="20px auto"
+            />
           </Slide>
-          <Slide transition={["zoom", "fade"]} bgColor="primary">
-            <Heading caps fit>Flexible Layouts</Heading>
-            <Layout>
-              <Fill>
-                <Heading size={4} caps textColor="secondary" bgColor="white" margin={10}>
-                  Left
-                </Heading>
-              </Fill>
-              <Fill>
-                <Heading size={4} caps textColor="secondary" bgColor="white" margin={10}>
-                  Right
-                </Heading>
-              </Fill>
-            </Layout>
-          </Slide>
-          <Slide transition={["slide"]} bgColor="black">
-            <BlockQuote>
-              <Quote>Wonderfully formatted quotes</Quote>
-              <Cite>Ken Wheeler</Cite>
-            </BlockQuote>
-          </Slide>
-          <Slide transition={["spin", "zoom"]} bgColor="tertiary">
-            <Heading caps fit size={1} textColor="primary">
-              Inline Markdown
-            </Heading>
-            <Markdown>
-              {`
-![Markdown Logo](${images.markdown.replace("/", "")})
-
-You can write inline images, [Markdown Links](http://commonmark.org), paragraph text and most other markdown syntax
-* Lists too!
-* With ~~strikethrough~~ and _italic_
-* And let's not forget **bold**
-              `}
-            </Markdown>
+          <Slide transition={["slide"]} bgColor="navy" textColor="primary">
+          <Heading size={1} capts fit textColor="primary">
+            Polymer Elements Catalogs
+          </Heading>
+            <List>
+              <Appear><ListItem>Paper Elements</ListItem></Appear>
+              <Appear><ListItem>Gold Elements</ListItem></Appear>
+              <Appear><ListItem>Iron Elements </ListItem></Appear>
+              <Appear><ListItem>Google Web Componenets</ListItem></Appear>
+            </List>
           </Slide>
           <Slide transition={["slide", "spin"]} bgColor="primary">
             <Heading caps fit size={1} textColor="tertiary">
-              Smooth
+            Polymer
             </Heading>
             <Heading caps fit size={1} textColor="secondary">
-              Combinable Transitions
+            Creating A Library Usable Acoss All Frameworks
             </Heading>
-          </Slide>
-          <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
-            <List>
-              <Appear><ListItem>Inline style based theme system</ListItem></Appear>
-              <Appear><ListItem>Autofit text</ListItem></Appear>
-              <Appear><ListItem>Flexbox layout system</ListItem></Appear>
-              <Appear><ListItem>PDF export</ListItem></Appear>
-              <Appear><ListItem>And...</ListItem></Appear>
-            </List>
-          </Slide>
-          <Slide transition={["slide"]} bgColor="primary">
-            <Heading size={1} caps fit textColor="tertiary">
-              Your presentations are interactive
-            </Heading>
-            <Interactive/>
-          </Slide>
-          <Slide transition={["slide"]} bgColor="primary"
-            notes="Hard to find cities without any pizza"
-          >
-            <Heading size={4} caps textColor="secondary" bgColor="white" margin={10}>
-              Pizza Toppings
-            </Heading>
-            <Layout>
-              <Table>
-                <thead>
-                  <TableRow>
-                    <TableHeaderItem/>
-                    <TableHeaderItem>2011</TableHeaderItem>
-                    <TableHeaderItem>2013</TableHeaderItem>
-                    <TableHeaderItem>2015</TableHeaderItem>
-                  </TableRow>
-                </thead>
-                <tbody>
-                  <TableRow>
-                    <TableItem>None</TableItem>
-                    <TableItem>61.8%</TableItem>
-                    <TableItem>39.6%</TableItem>
-                    <TableItem>35.0%</TableItem>
-                  </TableRow>
-                  <TableRow>
-                    <TableItem>Pineapple</TableItem>
-                    <TableItem>28.3%</TableItem>
-                    <TableItem>54.5%</TableItem>
-                    <TableItem>61.5%</TableItem>
-                  </TableRow>
-                  <TableRow>
-                    <TableItem>Pepperoni</TableItem>
-                    <TableItem/>
-                    <TableItem>50.2%</TableItem>
-                    <TableItem>77.2%</TableItem>
-                  </TableRow>
-                  <TableRow>
-                    <TableItem>Olives</TableItem>
-                    <TableItem/>
-                    <TableItem>24.9%</TableItem>
-                    <TableItem>55.9%</TableItem>
-                  </TableRow>
-                </tbody>
-              </Table>
-            </Layout>
           </Slide>
           <Slide transition={["spin", "slide"]} bgColor="tertiary">
-            <Heading size={1} caps fit lineHeight={1.5} textColor="primary">
-              Made with love in Seattle by
+            <Heading size={3} caps fit lineHeight={1.5} textColor="primary">
+            This has been A presenation made By Natalia, help from:
             </Heading>
             <Link href="http://www.formidable.com"><Image width="100%" src={images.logo}/></Link>
           </Slide>
